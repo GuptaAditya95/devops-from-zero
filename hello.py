07d5e852effa09af228beadc8d82e6ee13c1bb94
EC2 Default User <ec2-user@ip-172-31-23-233.ap-southeast-2.compute.internal>
2024-02-04 18:06:08 +0000

    Added the other files

--- hello.py
+++ hello.py
@@ -2,18 +2,18 @@ import click
 import boto3
 
 
-def add(x,y):
-    return x+y
-    
+def add(x, y):
+    return x + y
+
+
 @click.command()
 def buckets():
     """This lists all my AWS buckets"""
-    
+
     s3 = boto3.client("s3")
     s3buckets = s3.list_buckets()
-    click.echo(
-            click.style(f"List of buckets: {s3buckets}", bg = 'white', fg = 'black')
-        )
+    click.echo(click.style(f"List of buckets: {s3buckets}", bg="white", fg="black"))
 
-if __name__ =="__main__":
+
+if __name__ == "__main__":
     buckets()
--- test_hello.py
+++ test_hello.py
@@ -1,7 +1,6 @@
 from hello import add
 
+
 # Add Debug Configurations
 def test_add():
-    assert 2 == add(1,1)
-    
-    
+    assert 2 == add(1, 1)
